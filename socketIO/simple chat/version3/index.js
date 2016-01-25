66d2c0d7471fb0435c8e12aa433a91e855a53998
fourtran <tranvantubk@gmail.com>
2016-01-25 17:26:39 +0700

    start again

--- socketIO/simple chat/version3/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 4444;
-var io = require('socket.io').listen(app.listen(port));
-    require('./config')(app, io);
-    require('./routes')(app, io);
-console.log('Server is listening on port '+port);
